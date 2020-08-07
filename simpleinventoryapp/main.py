1abe4f55bd9caac31379de77eb578c622f2c5e14
sambyers <samwbyers@gmail.com>
2020-08-06 20:26:30 -0400

    simple api for inventory

---
+++ simpleinventoryapp/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    ip: str
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "devnet example api"}
+
+
+@app.get("/inventory/{item}")
+async def get_inventory(item: str):
+    return {"item": item}
+
+@app.post("/inventory/")
+async def add_inventory(item: Item):
+    if item.description:
+        return {"created": item}
+    else:
+        item.description = f'{item.name} - {item.ip}'
+        return {"created": item}
